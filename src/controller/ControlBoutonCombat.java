cd0f7844005adbf2a2695bd424155c45898040ac
cpertuis <charly.pertuiset@outlook.com>
2017-01-13 00:12:33 +0100

    Merge branch 'master' of /home/charly/Bureau/ProjetTutS3/ProjetTutJME_S3 with conflicts.

--- src/controller/ControlBoutonCombat.java
+++ src/controller/ControlBoutonCombat.java
@@ -9,27 +9,28 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class ControlBoutonCombat implements ActionListener{
-  public ModelHero mHero;
-  public ModelHero mMob;
+    public ModelHero mHero;
+    public ModelHero mMob;
   
-  public ControlButtonFenetre(ModelHero mHero,ModelHero mMob,FenetreJeu fenetreJeu){
-    this.fenetreJeu = fenetreJeu;
-    this.mHero=mHero;
-    this.mMob=mMob;
-    this.fenetre.setInteraction(this);
-  }
+    public ControlButtonFenetre(ModelHero mHero,ModelHero mMob,FenetreJeu fenetreJeu){
+        this.fenetreJeu = fenetreJeu;
+        this.mHero=mHero;
+        this.mMob=mMob;
+        this.fenetre.setInteraction(this);
+    }
+
     public void actionPerformed(ActionEvent actionEvent) {
-      if (actionEvent.getActionCommand().equals("Attaque Physique")){
-      //To do
-      }
-      if (actionEvent.getActionCommand().equals("Attaque Magique")){
-      //To do
-      }
-      if (actionEvent.getActionCommand().equals("Defense Physique")){
-      //To do
-      }
-      if (actionEvent.getActionCommand().equals("Defense Magique")){
-      //To do
-      }
+        if (actionEvent.getActionCommand().equals("Attaque Physique")){
+        //To do
+        }
+        if (actionEvent.getActionCommand().equals("Attaque Magique")){
+        //To do
+        }
+        if (actionEvent.getActionCommand().equals("Defense Physique")){
+        //To do
+        }
+        if (actionEvent.getActionCommand().equals("Defense Magique")){
+        //To do
+        }
     }
 }
