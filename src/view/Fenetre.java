21001f2d7ba1294dff37e4694b0f9ca695063eda
cpertuis <charly.pertuiset@outlook.com>
2017-01-12 23:39:29 +0100

    Modif Fenetre

--- src/view/Fenetre.java
+++ src/view/Fenetre.java
@@ -11,9 +11,6 @@ import java.awt.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
-
-
-
 public class Fenetre extends JFrame {
 
     //public ControlButton controlBut;
@@ -23,14 +20,11 @@ public class Fenetre extends JFrame {
 	protected JButton defPhy;
 	protected JButton defMag;
 
-
-
-
     public Fenetre(ModelMob monstre, ModelHero hero) {
 		setTitle("Combat");
 		setResizable(true);
 		initAttribute();
-	
+
 		creerWidget(monstre, hero);
 		setSize(900,600);                                // Fixe la taille par défaut
 		setVisible(true);                                // Affiche la fenetre
@@ -38,7 +32,7 @@ public class Fenetre extends JFrame {
     }
 
 
-    public void creerWidget(ModelMob monstre, ModelHero hero) {    	
+    public void creerWidget(ModelMob monstre, ModelHero hero) {
     	ImageIcon icon = new ImageIcon(new ImageIcon("mon_image.jpg").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
     	JLabel image = new JLabel( icon);
     	JLabel vieJoueur = new JLabel(hero.getVie()+"/"+hero.getVieMax());
@@ -55,57 +49,56 @@ public class Fenetre extends JFrame {
         ligne2.setBackground(Color.WHITE);
         ligne2.add(attaquePhy);
         ligne2.add(attaqueMag);
-        
+
         JPanel ligne3 = new JPanel();
         ligne3.setLayout(new BoxLayout(ligne3, BoxLayout.LINE_AXIS));
         ligne3.add(defPhy);
         ligne3.add(defMag);
-        
+
         JPanel panNom = new JPanel();
         panNom.setBackground(Color.white);
         panNom.setPreferredSize(new Dimension(300, 100));
         panNom.setBorder(BorderFactory.createTitledBorder("Combat"));
         JLabel nomLabel = new JLabel(monstre.getNom());
         panNom.add(nomLabel);
-            
+
         JPanel global = new JPanel();
 	    global.setPreferredSize(new Dimension(900, 600));
 	    global.setBackground(Color.WHITE);
 	    global.setLayout(new GridBagLayout());
-	    
+
 	    GridBagConstraints gbc = new GridBagConstraints();
-	    	    
+
 	    gbc.gridx = 2;
 	    gbc.gridy = 1;
 	    gbc.gridheight = 1;
 	    gbc.gridwidth = 1;
 	    global.add(image, gbc);
-	    
+
 	    gbc.gridx = 4;
 	    gbc.gridy = 1;
 	    gbc.gridheight = 1;
 	    gbc.gridwidth = 1;
 	    global.add(image2, gbc);
-	    
+
 	    gbc.gridx = 3;
 	    gbc.gridy = 3;
 	    gbc.gridheight = 1;
 	    gbc.gridwidth = 1;
 	    global.add(panNom, gbc);
-	    
+
 	    gbc.gridx = 3;
 	    gbc.gridy = 5;
 	    gbc.gridheight = 1;
 	    gbc.gridwidth = 1;
 	    global.add(ligne2, gbc);
-	    
+
 	    gbc.gridx = 4;
 	    gbc.gridy = 2;
 	    gbc.gridheight = 1;
 	    gbc.gridwidth = 1;
 	    global.add(vieJoueur, gbc);
-	    
-	    
+
 	    gbc.gridx = 2;
 	    gbc.gridy = 2;
 	    gbc.gridheight = 1;
@@ -118,9 +111,9 @@ public class Fenetre extends JFrame {
 
     }
     public static void main(String[] args) {
-    	 
+
         javax.swing.SwingUtilities.invokeLater( new Runnable() {
-     
+
           public void run() {
         	  ModelMob monstre = new ModelMob("paul");
         	  ModelHero hero = new ModelHero("raph", ModelEntite.Genre.HOMME,4);
@@ -130,4 +123,3 @@ public class Fenetre extends JFrame {
       }
 
 }
-
